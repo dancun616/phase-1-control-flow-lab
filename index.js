07c5aafb83656b20332acb291a4c47bf49a545ec
dancun616 <dancun616@gmail.com>
2022-11-23 17:27:20 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 function scuberGreetingForFeet(){
-  // Write your code here!
+  if (scuberGreetingForFeet > 200){
+    result = scuberGreetingForFeet + "Hi am here"
+  }else if (scuberGreetingForFeet == 200){
+     result = "error try again"
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
+function ternaryCheckCity(city) {
   // Write your code here!
+  const cityReply= city==='NYC'? "Ok, sounds good." : "No go.";
+  return cityReply;
+ }
+ console.log(ternaryCheckCity('NYC'));
+ console.log(ternaryCheckCity('Pittsburgh'));
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  let tipReply;
+  switch(tip) {
+    case 'generous':
+      return (tipReply=('Thank you so much.'));
+    case 'not as generous':
+      return(tipReply=('Thank you.'));
+    default:
+      return(tipReply=('Bye.'));
+  }
 }
+console.log(switchOnCharmFromTip('generous'));
+console.log(switchOnCharmFromTip('not as generous'));
+console.log(switchOnCharmFromTip('Thank you for everything'));
